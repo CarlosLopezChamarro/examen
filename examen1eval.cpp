5f3f42806352068d95b992a37c691d273462a9fe
CarlosLopezChamarro <43232748+CarlosLopezChamarro@users.noreply.github.com>
2018-11-29 09:58:23 +0100

    Add files via upload

---
+++ examen1eval.cpp
@@ -0,0 +1,14 @@
+//este programa sirve para
+#include<iostream> 
+int main();
+          std::cout<<("Enter first number: ")
+          std::cout<<("Enter second number: ")
+          std::cout<<("Enter third number: ")
+          std::cin>> (num1 >= num2) and (num1 >= num3):
+          std::cin>> (x = num1)
+
+     if (num2>=num1) and (num>=num3):
+     x = num2
+else:
+ x= num3
+print "",x
